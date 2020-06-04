f61006c91dd5d57b6beed397317030d44a28b3a3
Vadim <vadim113512@tut.by>
2020-06-04 21:52:03 +0300

    init: add files Homework hw7

---
+++ src/hw7/Students.java
@@ -0,0 +1,22 @@
+package hw7;
+
+public class Students {
+    private int Id;
+    private String Name;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
